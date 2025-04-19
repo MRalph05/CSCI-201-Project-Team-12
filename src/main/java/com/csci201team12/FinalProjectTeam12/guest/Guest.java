1b3b98539b8ba33802e666a44b715ec0239e8592
Towhid Aziz <towhidaz@usc.edu>
2025-04-18 20:57:21 -0700

    base for Spring Boot

---
+++ src/main/java/com/csci201team12/FinalProjectTeam12/guest/Guest.java
@@ -0,0 +1,29 @@
+package com.csci201team12.FinalProjectTeam12.guest;
+
+public class Guest {
+    int guestID;
+
+    Guest() {
+        // TODO: create new entry in database, guestID == lastID + 1
+    }
+
+    void createTask(String taskName) {
+        // TODO
+    }
+
+    void completeTask(String taskName) {
+        // TODO
+    }
+
+    void createRoom() {
+        // TODO
+    }
+
+    void deleteDuest() {
+        // TODO
+    }
+
+    void addTutorialTask() {
+        // TODO: predefined task
+    }
+}
---
+++ src/test/java/com/csci201team12/FinalProjectTeam12/FinalProjectTeam12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csci201team12.FinalProjectTeam12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectTeam12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
