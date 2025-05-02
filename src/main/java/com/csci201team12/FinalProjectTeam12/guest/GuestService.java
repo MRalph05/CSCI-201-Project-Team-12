3515cae97aa73f30ba2452d4e7e05e7ee34aeec5
peeruaakanksha <peeru@usc.edu>
2025-05-02 15:22:59 -0700

    minor guest changes

--- src/main/java/com/csci201team12/FinalProjectTeam12/guest/GuestService.java
+++ src/main/java/com/csci201team12/FinalProjectTeam12/guest/GuestService.java
@@ -21,26 +21,38 @@ public class GuestService {
     public void createTask(int guestID, String taskName) {
         Guest guest = getGuestOrThrow(guestID);
         String current = guest.getGuestTasks();
-        if (!current.isEmpty()) current += ",";
-        guest.setGuestTasks(current + taskName);
+//        if (!current.isEmpty()) current += ",";
+        if(current!=null && Arrays.asList(current.split(",")).contains(taskName)) {
+            throw new IllegalArgumentException("Task name already exists");
+        }
+        if(current==null || current.isBlank()) {
+            guest.setGuestTasks(taskName);
+        }
+        else{
+            guest.setGuestTasks(current+","+taskName);
+        }
+//        guest.setGuestTasks(current + taskName);
         guestRepo.save(guest);
+    }
+    private String removeTask(String tasks, String task){
+        return Arrays.stream(tasks.split(",")).map(String::trim).collect(Collectors.joining(","));
     }
 
     public void completeTask(int guestID, String taskName) {
         Guest guest = getGuestOrThrow(guestID);
-        String updatedTasks = Arrays.stream(guest.getGuestTasks().split(","))
-                .filter(task -> !task.trim().equals(taskName))
-                .collect(Collectors.joining(","));
-        guest.setGuestTasks(updatedTasks);
+//        String updatedTasks = Arrays.stream(guest.getGuestTasks().split(","))
+//                .filter(task -> !task.trim().equals(taskName))
+//                .collect(Collectors.joining(","));
+        guest.setGuestTasks(removeTask(guest.getGuestTasks(), taskName));
         guestRepo.save(guest);
     }
     
     public void deleteTask(int guestID, String taskName) {
         Guest guest = getGuestOrThrow(guestID);
-        String updatedTasks = Arrays.stream(guest.getGuestTasks().split(","))
-                .filter(task -> !task.trim().equals(taskName))
-                .collect(Collectors.joining(","));
-        guest.setGuestTasks(updatedTasks);
+//        String updatedTasks = Arrays.stream(guest.getGuestTasks().split(","))
+//                .filter(task -> !task.trim().equals(taskName))
+//                .collect(Collectors.joining(","));
+        guest.setGuestTasks(removeTask(guest.getGuestTasks(), taskName));
         guestRepo.save(guest);
     }
 
